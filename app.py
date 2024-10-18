48fc8e660ae1107d7f634c1c07120730e064af31
OBD <dhqudeh1@gmail.com>
2024-10-18 16:49:15 +0900

    flask initialize

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template, request, jsonify
+import os
+
+app = Flask(__name__)
+
+# Load environment variables from .env
+app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/api/data', methods=['POST'])
+def api_data():
+    data = request.json
+    return jsonify({"response": "Received", "data": data})
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5050, debug=True)

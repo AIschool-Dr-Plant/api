4e8dce3760158bc0e2a543376d1717d368a1079b
OBD <dhqudeh1@gmail.com>
2024-10-19 19:45:34 +0900

    auto deployment update

--- app.py
+++ app.py
@@ -10,10 +10,12 @@ app.config['SECRET_KEY'] = os.getenv('SECRET_KEY', 'default_secret_key')
 def home():
     return render_template('index.html')
 
-@app.route('/api/data', methods=['POST'])
+@app.route('/api/data', methods=['GET'])
 def api_data():
-    data = request.json
+    # 쿼리 파라미터를 딕셔너리 형태로 변환
+    data = dict(request.args)
+    # JSON 응답으로 반환
     return jsonify({"response": "Received", "data": data})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5050)
